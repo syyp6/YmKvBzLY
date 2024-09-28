4b5c0ee078e2fcdec8c364f3a03668e3a912ef0a
syyp6 <112552038+syyp6@users.noreply.github.com>
2024-09-29 00:46:44 +0530

    i want to commit this file

---
+++ .sync.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+cnpm sync @elog/cli
+cnpm sync @elog/core
+cnpm sync @elog/deploy
+cnpm sync @elog/plugin-adapter
+cnpm sync @elog/plugin-image
+cnpm sync @elog/sdk-confluence
+cnpm sync @elog/sdk-flowus
+cnpm sync @elog/sdk-feishu
+cnpm sync @elog/sdk-halo
+cnpm sync @elog/sdk-notion
+cnpm sync @elog/sdk-wolai
+cnpm sync @elog/sdk-wordpress
+cnpm sync @elog/sdk-yuque
+cnpm sync @elog/shared
+cnpm sync @elog/types
